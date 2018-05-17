c37087f81f731163e93c7f3ed1dc17dc34302e79
Jashan Bhoora <jashanbhoora@users.noreply.github.com>
2018-05-17 16:33:35 +0100

    Implemented toggling of data layers on map

--- gpigapp/static/js/main.js
+++ gpigapp/static/js/main.js
@@ -1,3 +1,7 @@
+var FLOOD_LAYER = "floodLayer";
+var BUILDINGS_LAYER = "affectedBuildingsLayer";
+var SAFEZONES_LAYER =  "safezonesLayer";
+
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 socket.on('connect', function()
 {
@@ -31,23 +35,56 @@ socket.on("updateTaskList", function(taskList)
 
 
 $( document ).ready(function() {
+    $("#checkbox-flood-area").change(function()
+    {
+        if(this.checked)
+        {
+            $("canvas#main").getLayer(FLOOD_LAYER).visible = true;
+        }
+        else
+        {
+            $("canvas#main").getLayer(FLOOD_LAYER).visible = false;
+        }
+        $("canvas#main").drawLayers();
+    });
 
+    $("#checkbox-affected-buildings").change(function()
+    {
+        if(this.checked)
+        {
+            $("canvas#main").getLayer(BUILDINGS_LAYER).visible = true;
+        }
+        else
+        {
+            $("canvas#main").getLayer(BUILDINGS_LAYER).visible = false;
+        }
+        $("canvas#main").drawLayers();
+    });
+
+    $("#checkbox-safe-zones").change(function()
+    {
+        if(this.checked)
+        {
+            $("canvas#main").getLayer(SAFEZONES_LAYER).visible = true;
+        }
+        else
+        {
+            $("canvas#main").getLayer(SAFEZONES_LAYER).visible = false;
+        }
+        $("canvas#main").drawLayers();
+    });
+
+    $("canvas#main").addLayer({name: FLOOD_LAYER, fillStyle: "rgba(41, 148, 219, 0.6)", type:"rectangle", x: 100, y: 100, width: 200, height: 250})
+        .addLayer({name: BUILDINGS_LAYER, fillStyle: "rgba(221, 17, 17, 0.6)", type:"rectangle", x: 300, y: 300, width: 200, height: 250})
+        .addLayer({name: SAFEZONES_LAYER, fillStyle: "rgba(5, 178, 14, 0.6)", type:"rectangle", x: 500, y: 500, width: 200, height: 250}).drawLayers();
 });
 
 //wait for the image to load before setting the canvas size and attempting to draw
 $("#beforeImage").on('load', function(){
     $("canvas#main")[0].width = $("#beforeImage").width();
     $("canvas#main")[0].height = $("#beforeImage").height();
-
-    $("canvas#main").drawArc(
-        {
-            fillStyle: "blue",
-            x: 100,
-            y: 100,
-            radius: 40
-        }
-    );
-  });
+    $("canvas#main").drawLayers();
+});
 
 
 
